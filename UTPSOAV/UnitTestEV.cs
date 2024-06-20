1d53c590f80c7ee5b62c059ada10e7c47a26fd55
shahid.here.business@gmail.com <shahid.here.business@gmail.com>
2024-06-20 17:36:49 +0500

    Environment Variable Immutable

--- UTPSOAV/UnitTestEV.cs
+++ UTPSOAV/UnitTestEV.cs
@@ -8,21 +8,27 @@ namespace UTPSOAV
 {
     [TestClass]
     public class UnitTestEV
-    {        
+    {
+        private readonly string evName = "EV_STATE";
+        private string context { get; set; }
         [TestMethod]
         public void TestMethod()
-        {
-            string evName = "EV_STATE";
-            // Read
-            string surrogate = EnvironmentState.EVFetch(evName, EnvironmentVariableTarget.Machine);
-
-            if (!string.IsNullOrEmpty(surrogate))
-                Console.WriteLine($"EV_STATE: {surrogate}");
-            // Create
-            surrogate = $"Hello World to Environment Variable State is {DateTime.Now.ToString("yyyyMMddHHmmssfffffff")}";
-            Console.WriteLine($"Created EV_STATE:{EnvironmentState.EVCreate(surrogate, evName, EnvironmentVariableTarget.Machine)}");
+        {            
+            // Read First State
+            context = EnvironmentState.EVFetch(evName, EnvironmentVariableTarget.Machine);
+            // Conditional Stuff
+            if (!string.IsNullOrEmpty(context))
+                Console.WriteLine($"EV_STATE: {context}");
+            // Create State
+            context = $"Hello World to Environment Variable State is {DateTime.Now.ToString("yyyyMMddHHmmssfffffff")}";
+            Console.WriteLine($"Created EV_STATE:{EnvironmentState.EVCreate(context, evName, EnvironmentVariableTarget.Machine)}");
+            // Update State
+            context = $"Hello World to Environment Variable State Update is {DateTime.Now.ToString("yyyyMMddHHmmssfffffff")}";
+            Console.WriteLine($"Created EV_STATE:{EnvironmentState.EVCreate(context, evName, EnvironmentVariableTarget.Machine)}");
+            // Read State
+            Console.WriteLine($"Created EV_STATE:{EnvironmentState.EVFetch(evName, EnvironmentVariableTarget.Machine)}");
             // Create for Delete
-            Console.WriteLine($"Created EV_STATE_DEL:{EnvironmentState.EVCreate(surrogate, $"{evName}_DEL", EnvironmentVariableTarget.Machine)}");
+            Console.WriteLine($"Created EV_STATE_DEL:{EnvironmentState.EVCreate(context, $"{evName}_DEL", EnvironmentVariableTarget.Machine)}");
             // Delete EV_STATE_DEL
             Console.WriteLine($"Deleted EV_STATE_DEL:{EnvironmentState.EVRemove($"{evName}_DEL", EnvironmentVariableTarget.Machine)}");
         }
