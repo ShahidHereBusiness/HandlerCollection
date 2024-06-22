e8e037f1772551b1e7a6bcf9524aaa65ec4b04d9
shahid.here.business@gmail.com <shahid.here.business@gmail.com>
2024-06-22 12:45:01 +0500

    IDisposable implementation for Unmanaged resources

---
+++ SOAV/ILogManager.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using static SOAV.Include.ExceptionMapping;
+using static SOAV.Include.Enumeration;
+using System.Web;
+
+namespace SOAV
+{
+    /// <summary>
+    /// Solution Developer:
+    /// IDisposable Interface Implementation upon
+    /// File System Logging and Backed by Application Event Viewer App
+    /// </summary>
+    public class ILogManager : IDisposable
+    {
+        private static readonly string RemoteAddress = Validation.RationalizePath(HttpContext.Current?.Request.ServerVariables["REMOTE_ADDR"] ?? "App");
+        /// <summary>
+        /// Traceability Identification
+        /// </summary>
+        private string TID { get; set; }
+        public ILogManager()
+        {
+            TID = $"{DateTime.Now.ToString("yyyyMMddHHmmssfffffff")}{new Random(Guid.NewGuid().GetHashCode()).Next()}";
+        }
+        /// <summary>
+        /// Solution Developer:
+        /// File System application logging
+        /// </summary>
+        /// <param name="path">Local Drive:\\Folder</param>
+        /// <param name="MethodName">Method of Log trace</param>
+        /// <param name="logMsg">Pipe delimated or Newtonjson Param Message</param>
+        /// <param name="ResponseMsg">Resultant Message Response</param>
+        /// <param name="IsResponse">False if Request or Receipt</param>
+        /// <param name="exp">Exception Details Logging</param>
+        /// <returns>-6:Input not recognised ,0:Success, -3:Error in Logging, 1: Unexpected Failure</returns>
+        public string FileSystemLog(string path, string MethodName, string logMsg, out string ResponseMsg, bool IsResponse = false, Exception exp = null)
+        {
+            ResponseMsg=string.Empty;
+            try
+            {
+                if (string.IsNullOrEmpty(path))
+                    ResponseMsg = ResponseEnum.FormatError.ToString();
+                path += $"{DateTime.Now.ToString("yyyy")}\\{DateTime.Now.ToString("MM")}\\{DateTime.Now.ToString("dd")}\\";
+                if (!path.Substring(path.Length - 1).Contains("\\"))
+                    ResponseMsg = ResponseEnum.FormatError.ToString();
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+                string path2 = $"{path}{DateTime.Now:yyyyMMddHH}_{RemoteAddress}.txt";
+                StreamWriter streamWriter = (File.Exists(path2) ? File.AppendText(path2) : File.CreateText(path2));
+                string expMsg = (exp != null) ? logMsg = $"{logMsg}{NewLine}{ExceptionDetails(exp)}" : string.Empty;
+                streamWriter.WriteLine($"{MethodName},{((IsResponse) ? "Response" : "Receipt")}|{logMsg}");
+                streamWriter.Close();
+            }
+            catch (Exception ex)
+            {
+                EventViewerAppLog($"{MethodName}{NewLine}{logMsg}{NewLine}{ex.Message}", out bool response, true, "Application", 5175, 101);
+                if (!response)
+                    ResponseMsg = ResponseEnum.FileSystemLogFailure.ToString();
+                ResponseMsg = ResponseEnum.UnexpectedFailure.ToString();
+            }
+            finally
+            {
+                ResponseMsg = string.IsNullOrEmpty(ResponseMsg) ? ResponseEnum.Success.ToString() : ResponseMsg;
+            }
+            return TID;
+        }
+        /// <summary>
+        /// Solution Developer:
+        /// Event Viewer Windows Log in Application
+        /// </summary>
+        /// <param name="logMsg">Row wise Param Msg</param>
+        /// <param name="ResponseMsg">Resultant true or false Response</param>
+        /// <param name="IsResponse">True or False Response</param>
+        /// <param name="source">Handle Event Source</param>
+        /// <param name="eventId">Event Identification Number</param>
+        /// <param name="category">Category Number</param>
+        /// <returns>true/false</returns>
+        public string EventViewerAppLog(string logMsg, out bool ResponseMsg, bool IsResponse = false, string source = "Application", int eventId = 5175, short category = 101)
+        {
+            try
+            {
+                if (!EventLog.SourceExists(source)) ResponseMsg = false;// Verify Event Registered
+                using (EventLog eventLog = new EventLog(source))//Log Name
+                {
+                    logMsg = $"{((IsResponse) ? "Response" : "Receipt")}|{logMsg}";
+                    eventLog.Source = source;//Source
+                    eventLog.WriteEntry(logMsg, EventLogEntryType.Information, eventId, category);
+                }
+                ResponseMsg = true;
+            }
+            catch
+            {
+                ResponseMsg = false;
+            }
+            return TID;
+        }
+        /// <summary>
+        /// Solution Developer:
+        /// Exception Handled Details Logging
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <returns>Exception string message</returns>
+        private static string ExceptionDetails(Exception ex)
+        {
+            ErrorLineNo = ex?.StackTrace?.Substring(ex.StackTrace.Length - 7, 7) ?? "";
+            ErrorMsg = ex?.GetType().Name.ToString() ?? "";
+            ExType = ex?.GetType().ToString() ?? "";
+            ExURL = RemoteAddress ?? "";
+            ErrorLocation = ex?.Message.ToString() ?? "";
+
+            string error = "-----------Exception Details Start-----------";
+            error += $"Log Written Date:{DateTime.Now.ToString("yyyyMMddHHmmssfff")}{NewLine}Error Line No :{ErrorLineNo}{NewLine}Error Message:{ErrorMsg}{NewLine}Exception Type:{ExType}{NewLine}Error Location :{ErrorLocation}{NewLine}Error Page Url:{ExURL}{NewLine}User Host IP:{HostAddress}{NewLine}";
+            error += "-----------Exception Details End-----------";
+            return error;
+        }
+        /// <summary>
+        /// Your Dispositions
+        /// </summary>
+        public void Dispose()
+        {
+            return;
+        }
+    }
+}
--- UTPSOAV/UnitTestLogging.cs
+++ UTPSOAV/UnitTestLogging.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SOAV;
 using System;
 using static SOAV.LogManager;
+using SOAV;
 
 namespace UTPSOAV
 {
@@ -15,7 +15,7 @@ namespace UTPSOAV
         public void TestMethod1FSLog()
         {
             int resultant;
-            string TimeLog = $"{DateTime.Now:yyyyMMddHHmmssfffffff}{new Random(Guid.NewGuid().GetHashCode()).Next()}";            
+            string TimeLog = $"{DateTime.Now:yyyyMMddHHmmssfffffff}{new Random(Guid.NewGuid().GetHashCode()).Next()}";
             // Missed Fodler Reference atleats one postfix "\\"
             resultant = FileSystemLog("UTP", "HelloWorld", "M|N|O|P");
             Console.WriteLine($"Resultant:{resultant}");// Return Overview
@@ -35,13 +35,63 @@ namespace UTPSOAV
         [TestMethod]
         public void TestMethod2ApplicationEventLog()
         {
-            Boolean flag;            
+            Boolean flag;
             // Request Logging
             flag = EventViewerAppLog("M|N|O|P");
             Console.WriteLine($"Flag:{flag}");// Return Overview
             // Response Logging
             flag = EventViewerAppLog("M|N|O|P|R", true);
             Console.WriteLine($"Flag:{flag}");// Return Overview
+        }
+        /// <summary>
+        /// File System IDisposable Implementation Logging
+        /// </summary>
+        [TestMethod]
+        public void TestMethod3FSLog()
+        {
+            string timeLog;
+
+            #region Shared TID
+            using (ILogManager log = new ILogManager())
+            {
+                // Missed Fodler Reference atleats one postfix "\\"
+                timeLog = log.FileSystemLog("UTP", "HelloWorld", "M|N|O|P", out string response);
+                Console.WriteLine($"Resultant:{response}, TID:{timeLog}");// Return Overview
+                // Request Logging
+                timeLog = log.FileSystemLog("UTP\\", "HelloWorld", "M|N|O|P", out response);
+                Console.WriteLine($"Resultant:{response}, TID:{timeLog}");// Return Overview
+                // Response Logging
+                timeLog = log.FileSystemLog("D:\\DEVLOGS\\UTP\\", "HelloWorld", "M|N|O|P|R", out response, true);
+                Console.WriteLine($"Resultant:{response}, TID:{timeLog}");// Return Overview
+            };
+            #endregion
+
+            #region New TID
+            using (ILogManager log = new ILogManager())
+            {
+                // Exception Logging                
+                timeLog = log.FileSystemLog("D:\\DEVLOGS\\UTP\\", "HelloWorld", $"{timeLog}|M|N|O|P|R", out string response, true, new Exception("Time Trace"));
+                Console.WriteLine($"Resultant:{response}, TID:{timeLog}");// Return Overview 
+            };
+            #endregion
+        }
+        /// <summary>
+        /// Windows Application IDisposable Implementation Logging
+        /// </summary>
+        [TestMethod]
+        public void TestMethod4ApplicationEventLog()
+        {
+            string timeLog;
+
+            using (ILogManager log = new ILogManager())
+            {
+                // Request Logging
+                timeLog = log.EventViewerAppLog("M|N|O|P", out bool flag);
+                Console.WriteLine($"Flag:{flag}, TID:{timeLog}");// Return Overview
+                // Response Logging
+                timeLog = log.EventViewerAppLog("M|N|O|P|R", out flag, true);
+                Console.WriteLine($"Flag:{flag}, TID:{timeLog}");// Return Overview
+            };
         }
     }
 }
